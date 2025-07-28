829744d4a58f28a0d26cf6b332a2f8379cb63726
howardsolutions <phungthinh0946519356@gmail.com>
2025-07-28 11:15:22 +0700

    feat: auth components

---
+++ components/auth/login-form.tsx
@@ -0,0 +1,15 @@
+import { CardWrapper } from "./card-wrapper"
+
+const LoginForm = () => {
+  return (
+    <CardWrapper headerLabel="Welcome back"
+        backButtonLabel="Don't have an account?"
+        backButtonHref="/auth/register"
+        showSocial
+    >
+        Login Form!
+    </CardWrapper>
+  )
+}
+
+export default LoginForm
