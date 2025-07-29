26897e57b9340ba48be134d13b227d2e35824539
howardsolutions <phungthinh0946519356@gmail.com>
2025-07-29 13:01:01 +0700

    feat: callback sessions and jwt, modify user schema with user role

--- auth.config.ts
+++ auth.config.ts
@@ -3,7 +3,7 @@ import Github from "next-auth/providers/github";
 import Credentials from "next-auth/providers/credentials";
 import { LoginSchema } from "@/schemas";
 import { getUserByEmail } from "@/data/user";
-import bcrypt from 'bcrypt';
+import bcrypt from "bcryptjs";
 
 export default {
   providers: [
--- auth.ts
+++ auth.ts
@@ -3,6 +3,8 @@ import authConfig from "./auth.config";
 
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { db } from "./lib/db";
+import { getUserById } from "./data/user";
+import { UserRole } from "@prisma/client";
 
 export const {
     handlers: { GET, POST },
@@ -12,5 +14,29 @@ export const {
 } = NextAuth({
     adapter: PrismaAdapter(db),
     session: { strategy: "jwt" },
+    callbacks: {
+        async session({ token, session }) {
+            if (token.sub && session.user) {
+                session.user.id = token.sub
+            }
+
+            if (token.role && session.user) {
+                session.user.role = token.role as UserRole;
+            }
+
+            return session
+        },
+        async jwt({ token }) {
+            if (!token.sub) return token;
+
+            const existingUser = await getUserById(token.sub)
+
+            if (!existingUser) return token;
+
+            token.role = existingUser.role;
+
+            return token
+        }
+    },
     ...authConfig
 });
